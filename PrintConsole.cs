157d04286cfe52fef9fd751fa7944a9905f5961c
chasukkung <155651351+chasukkung@users.noreply.github.com>
2024-01-05 00:09:29 +0900

    first C# test
    Debug.log test.

---
+++ PrintConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("oh,i'm too fat. i have to diet..");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
